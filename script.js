8c1ae0a36603f9059328c76c5ca51119e152eb56
APomke <3112520587@qq.com>
2023-08-06 20:17:06 +0800

    添加了构建ec2页面功能

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const createSecurityGroupButton = document.getElementById('createSecurityGroup');
+const createEC2InstanceButton = document.getElementById('createEC2Instance'); 
 
 createSecurityGroupButton.addEventListener('click', function() {
   window.location.href = 'securitygroup/securitygroup.html';
 });
 
+createEC2InstanceButton.addEventListener('click', function() {
+  window.location.href = 'ec2/ec2.html';
+});
